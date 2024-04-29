498ed62e11b8c1bab458870cb5eebe277ae1c1c8
Algonight <benjamin.fournet99@gmail.com>
2024-04-29 11:41:33 +0200

    Add empty core components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header/header.component';
+import { CalculatorInputComponent } from './calculator/calculator-input/calculator-input.component';
+import { CalculatorResultComponent } from './calculator/calculator-result/calculator-result.component';
+import { ConditionalDetailsComponent } from './details/conditional-details/conditional-details.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CalculatorInputComponent, CalculatorResultComponent, ConditionalDetailsComponent],
   templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {}
---
+++ src/app/details/conditional-details/conditional-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conditional-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './conditional-details.component.html',
+  styleUrl: './conditional-details.component.scss',
+})
+export class ConditionalDetailsComponent {}
