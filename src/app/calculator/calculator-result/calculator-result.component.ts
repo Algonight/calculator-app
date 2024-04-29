971112f81dbb8167efaf36b68cebafbf58305a05
Algonight <benjamin.fournet99@gmail.com>
2024-04-29 15:15:15 +0200

    Fill calculator result component

--- src/app/calculator/calculator-result/calculator-result.component.ts
+++ src/app/calculator/calculator-result/calculator-result.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-calculator-result',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './calculator-result.component.html',
   styleUrl: './calculator-result.component.scss',
 })
 export class CalculatorResultComponent {
-  @Input() result?: number;
+  private _result?: number;
+  resultSquareRoot?: number;
+
+  get result(): number | undefined {
+    return this._result;
+  }
+
+  @Input() set result(value: number | undefined) {
+    if (value === undefined) return;
+    this._result = value;
+    this.resultSquareRoot = Math.sqrt(value);
+  }
 }
